31bf79571f160dd4f09844a5aef12cfab52102e1
Kawã Kleber <kawaklebersc@gmail.com>
2025-11-16 23:44:02 -0300

    Build Initial

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,10 @@
+import React from 'react';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
-
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import '@/global.css';
 
 export const unstable_settings = {
   anchor: '(tabs)',
