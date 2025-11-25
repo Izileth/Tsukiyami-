c60d0d90efc1cbce9fd02d479f9daf9fefb25085
Kawã Kleber <kawaklebersc@gmail.com>
2025-11-25 20:32:23 -0300

    Update Build Packs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,22 +1,21 @@
 import 'react-native-url-polyfill/auto';
-import React, { useState, useEffect } from 'react';
+import React, {  useEffect } from 'react';
 import { Stack, useRouter, useSegments } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import * as SplashScreen from 'expo-splash-screen';
 import '@/global.css';
 import '@/utils/polyfills'; // se tiver alias configurado
 
-import { supabase } from '@/utils/supabase';
+
 import { AuthProvider, useAuth } from '@/context/AuthContext';
 import { ProfileProvider } from '@/context/ProfileContext';
 import { PostsProvider } from '@/context/PostsContext';
 import { CommentsProvider } from '@/context/CommentsContext';
 import { ReactionsProvider } from '@/context/LikesContext';
-import LoadingScreen from './_loading';
+
 import GlobalHeader from '@/components/global-header';
 import Toast from 'react-native-toast-message';
 import { toastConfig } from '@/components/ui/toasts';
-import { registerForPushNotificationsAsync } from '@/utils/notifications';
 
 // Keep the native splash screen visible
 SplashScreen.preventAutoHideAsync();
@@ -58,28 +57,8 @@ function RootLayoutNav() {
 }
 
 export default function RootLayout() {
-  const [isSplashFinished, setIsSplashFinished] = useState(false);
-  const [isAppReady, setIsAppReady] = useState(false);
-
   useEffect(() => {
-    async function prepare() {
-      try {
-        await registerForPushNotificationsAsync();
-        // This is where you would load fonts, assets, etc.
-        // We also add a minimum delay to see the splash screen.
-        await Promise.all([
-          supabase.auth.getSession(), // Ensures session is loaded before AuthProvider tries
-          new Promise(resolve => setTimeout(resolve, 10000)),
-        ]);
-      } catch (e) {
-        console.warn('App preparation error:', e);
-      } finally {
-        setIsAppReady(true);
-        await SplashScreen.hideAsync();
-      }
-    }
-
-    prepare();
+    SplashScreen.hideAsync();
   }, []);
 
   return (
@@ -88,18 +67,9 @@ export default function RootLayout() {
         <PostsProvider>
           <CommentsProvider>
             <ReactionsProvider>
-                {!isSplashFinished ? (
-                  <LoadingScreen
-                    isAppReady={isAppReady}
-                    onExitAnimationFinish={() => setIsSplashFinished(true)}
-                  />
-                ) : (
-                  <>
-                    <GlobalHeader />
-                    <RootLayoutNav />
-                    <Toast config={toastConfig} />
-                  </>
-                )}
+              <GlobalHeader />
+              <RootLayoutNav />
+              <Toast config={toastConfig} />
             </ReactionsProvider>
           </CommentsProvider>
         </PostsProvider>
